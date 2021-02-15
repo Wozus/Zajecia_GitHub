a5a84987d31779165f8168c7a56c7b1001e9351a
Wozus <236015@edu.p.lodz.pl>
2021-02-15 13:07:20 +0100

    2. Dodano  funkcje mnozenie

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int mnozenie (int a, int b)
+{
+    return a*b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    for(int i=0;i<10;i++)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    for(int i=1,j=1;i<=5 && j<=10;i++,j+=2)
+    {
+        printf("%d ",mnozenie(i,j));
+    }
     return 0;
 }
