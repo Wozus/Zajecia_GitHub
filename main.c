7bbac17777c9988c95102fe6d85111dd4d9073dc
Wozus <236015@edu.p.lodz.pl>
2021-02-15 13:10:28 +0100

    3. Funckja dodawanie

--- main.c
+++ main.c
@@ -4,7 +4,10 @@ int mnozenie (int a, int b)
 {
     return a*b;
 }
-
+int dodawanie(int a,b)
+{
+    return a+b;
+}
 int main() {
     printf("Hello, World!\n");
     for(int i=0;i<10;i++)
@@ -14,7 +17,8 @@ int main() {
     printf("\n");
     for(int i=1,j=1;i<=5 && j<=10;i++,j+=2)
     {
-        printf("%d ",mnozenie(i,j));
+        printf("%d %d\n",dodawanie(i,j),mnozenie(i,j));
     }
+
     return 0;
 }
